73660f1b68a0175180d2e1f905355778bdb9a90d
usuario <usuario@192.168.1.69>
2019-07-18 22:42:08 -0700

    update

--- src/main/java/com/aequilibrium/springboot/validation/TransformerValidation.java
+++ src/main/java/com/aequilibrium/springboot/validation/TransformerValidation.java
@@ -5,7 +5,7 @@ import com.aequilibrium.springboot.element.Transformer;
 
 public class TransformerValidation {
 	
-	private boolean transformerValid;
+	private boolean transformerValid = true;
 	private String message = "";
 	private Transformer transformer;
 	
@@ -29,63 +29,63 @@ public class TransformerValidation {
 	
 	private void validName() {
 		if (transformer.getName().isEmpty() || transformer.getName() == null) {
-			transformerValid = false;
+			setTransformerValid(false);
 			message.concat(TransformerConstant.NAME_NULL_EMPTY);
 		}
 	}
 	
 	private void validStrength() {
 		if (transformer.getStrenght() <= 0 || transformer.getStrenght() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.STRENGTH_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validIntelligence() {
 		if (transformer.getIntelligence() <= 0 || transformer.getIntelligence() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.INTELLIGENCE_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validSpeed() {
 		if (transformer.getSpeed() <= 0 || transformer.getSpeed() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.SPEED_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validEndurance() {
 		if (transformer.getEndurance() <= 0 || transformer.getEndurance() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.ENDURANCE_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validRank() {
 		if (transformer.getRank() <= 0 || transformer.getRank() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.RANK_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validCourage() {
 		if (transformer.getCourage() <= 0 || transformer.getCourage() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.COURAGE_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validFirePower() {
 		if (transformer.getFirepower() <= 0 || transformer.getFirepower() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.FIREPOWER_ONE_TEN).concat(" "));
 		}
 	}
 	
 	private void validSkill() {
 		if (transformer.getSkill() <= 0 || transformer.getSkill() > 10) {
-			transformerValid = false;
+			setTransformerValid(false);
 			setMessage(getMessage().concat(TransformerConstant.SKILL_ONE_TEN).concat(" "));
 		}
 	}
